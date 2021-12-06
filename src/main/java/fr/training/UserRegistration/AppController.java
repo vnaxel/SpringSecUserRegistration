bed38b0a0822aa5cd86d364e065be67dfe5b6653
yveno <yveno@LAPTOP-OACH3SV8>
2021-12-06 12:50:01 +0100

    created userList endpoint + registration now encrypt user password

--- src/main/java/fr/training/UserRegistration/AppController.java
+++ src/main/java/fr/training/UserRegistration/AppController.java
@@ -1,6 +1,9 @@
 package fr.training.UserRegistration;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,8 +30,19 @@ public class AppController {
 	
 	@PostMapping("/process_register")
 	public String processRegistration(User user){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String encodedPassord = encoder.encode(user.getPassword());
+		user.setPassword(encodedPassord);
 		repository.save(user);
 		return "register_success";
 	}
+	
+	@GetMapping("/list_users")
+	public String viewUsersList(Model model) {
+		List<User> listUsers = repository.findAll();
+		model.addAttribute("listUsers", listUsers);
+		return "users";
+	}
+	
 
 }
