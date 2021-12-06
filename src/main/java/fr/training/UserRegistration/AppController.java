775508d3e26981206368a0dc5097df4aaa86fe5c
yveno <yveno@LAPTOP-OACH3SV8>
2021-12-06 11:21:44 +0100

    view + controller for registration

--- src/main/java/fr/training/UserRegistration/AppController.java
+++ src/main/java/fr/training/UserRegistration/AppController.java
@@ -1,14 +1,34 @@
 package fr.training.UserRegistration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
 	
+	@Autowired
+	private UserRepository repository;
+	
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String showSignUpForm(Model model) {
+		
+		model.addAttribute("user", new User());
+		
+		return "signup_form";
+	}
+	
+	@PostMapping("/process_register")
+	public String processRegistration(User user){
+		repository.save(user);
+		return "register_success";
 	}
 
 }
